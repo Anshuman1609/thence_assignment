50a6f65bdf2dd12a25b5d305bb1a94dc99824ee2
Anshuman Dubey <anshumandubey1609@gmail.com>
2024-05-07 19:37:22 +0530

    structured the home page

---
+++ src/components/quest/Quest.js
@@ -0,0 +1,10 @@
+import "./quest.css";
+import React from 'react'
+
+const Quest = () => {
+    return (
+        <div>Quest</div>
+    )
+}
+
+export default Quest
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./home.css";
+import Navbar from "../../components/navbar/Navbar.js";
+import Header from "../../components/header/Header.js";
+import Quest from "../../components/quest/Quest.js";
+import Footer from "../../components/footer/Footer.js";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+            <Header />
+            <div>
+                <Quest />
+            </div>
+            <Footer />
+        </>
     )
 }
 

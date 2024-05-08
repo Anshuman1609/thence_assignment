a64ea1227945725d357214d71b5ae083d8703011
Anshuman Dubey <anshumandubey1609@gmail.com>
2024-05-09 00:48:24 +0530

    done with footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="fItem">
+        <div className="firstFooterItem">&copy; &nbsp; Talup 2023. All rights reserved</div>
+        <div className="secondFooterItem">
+          <span className="terms">Terms & Conditions</span>
+          <span className="policy">Privacy Policy</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
